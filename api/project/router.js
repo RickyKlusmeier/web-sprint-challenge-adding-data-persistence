c793b2501ed825cd9ac81ac704149cf9222e6f9f
Ricky Klusmeier <rickyklusmeier@gmail.com>
2021-05-14 13:24:50 -0600

    codegrade

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,24 @@
 // build your `/api/projects` router here
+const router = require("express").Router()
+
+const {logger, checkProjectPayload} = require('./middleware')
+const Projects = require('./model')
+
+
+router.get('/', logger, (req, res, next) => {
+    Projects.getAll(req.query)
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => next(err))
+})
+
+router.post('/', logger, checkProjectPayload, (req, res, next) => {
+    Projects.addProject(req.body)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(next)
+})
+ 
+ module.exports = router 
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,22 @@
 // build your `/api/tasks` router here
+const router = require("express").Router()
+const {logger, checkTaskPayload} = require('./middleware')
+const Tasks = require('./model')
+
+router.get('/', logger, (req, res, next) => {
+    Tasks.getAll(req.query)
+    .then(tasks => {
+        res.status(200).json(tasks)
+    })
+    .catch(err => next(err))
+})
+
+router.post('/', logger, checkTaskPayload, (req, res, next) => {
+    Tasks.addTask(req.body)
+    .then(task => {
+        res.status(200).json(task)
+    })
+    .catch(next)
+})
+
+module.exports = router 
